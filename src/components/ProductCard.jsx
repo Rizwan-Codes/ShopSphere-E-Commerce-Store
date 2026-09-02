e7afcdb2ca92c0be009916ca86fd53af1fb569bf
Rizwan <imrizwan164@gmail.com>
2026-09-02 11:15:48 +0500

    feat: ProductCard adds as a component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+function ProductCard({ product }) {
+   return (
+      <article className="group overflow-hidden border border-slate-200 rounded-3xl bg-white shadow-sm transition-shadow duration-300 hover:shadow-xl hover:shadow-slate-200/70">
+         <Link
+            to={`/NotFound`}
+            className="relative block overflow-hidden bg-slate-100"
+         >
+            <img src={product.image}
+               className="h-64 w-full object-cover"
+            />
+            <div className=" rounded-full absolute top-4 left-4 fonta border border-white/30 bg-white/90 px-3 py-1.5 text-xs font-bold text-slate-700 shadow-sm backdrop-blur">
+               {product.category}
+            </div>
+            <div className="fonta rounded-full absolute top-4 right-4  bg-slate-950/80 px-3 py-1.5 text-xs font-bold text-white shadow-sm backdrop-blur">
+               ★ {product.rating}
+            </div>
+         </Link>
+         <div className="p-5">
+
+            <Link
+               to={`/NotFound`}
+               className="font-medium text-[18px] line-clamp-2 min-h-20 leading-7 text-slate-900 transition-colors hover:text-blue-600"
+            >
+               {product.title}
+            </Link>
+
+            <div className="flex items-center justify-between gap-3 fonta">
+               <span className="text-xl font-black text-slate-900 tracking-widest">
+                  ${product.price.toFixed(2)}
+               </span>
+
+               <Link
+                  to={`/products`}
+                  className="text-sm font-normal text-blue-500 transition hover:text-blue-700"
+               >
+                  View details
+               </Link>
+            </div>
+            <button
+              className="text-bold fonta text-white text-center p-3 w-full rounded-xl mt-3 bg-slate-950 transition hover:bg-blue-600"
+            >
+               Add to Cart
+            </button>
+         </div>
+      </article>
+   )
+}
+
+export default ProductCard;
