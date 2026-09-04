5df9e1be818464413528ec6590196686cec5e5ba
Rizwan <imrizwan164@gmail.com>
2026-09-04 08:43:24 +0500

    feat: Products updated

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,7 @@ const products = [
     price: 109.95,
     description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+    image: "https://i.pinimg.com/1200x/ee/ef/8a/eeef8a357f2ed58a77d3c63c658816ce.jpg",
     rating: 3.9,
   },
   {
@@ -14,25 +14,25 @@ const products = [
     price: 22.3,
     description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+    image: "https://i.pinimg.com/1200x/57/ce/6d/57ce6d812cf064df6d54a94e5c0d6ff9.jpg",
     rating: 4.1,
   },
   {
     id: 3,
     title: "Mens Cotton Jacket",
     price: 55.99,
-    description: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    description: "great outerwear jackets for Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+    image: "https://i.pinimg.com/736x/8d/78/30/8d7830d7103a92c9a5406d18c7d52aea.jpg",
     rating: 4.7,
   },
   {
     id: 4,
-    title: "Mens Casual Slim Fit",
+    title: "Mens Casual Polo Tshirt",
     price: 15.99,
     description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png",
+    image: "https://i.pinimg.com/736x/21/c4/7a/21c47aeff70369b07ea31bb8bc14793a.jpg",
     rating: 2.1,
   },
   {
@@ -41,7 +41,7 @@ const products = [
     price: 695,
     description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
     category: "jewelery",
-    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_t.png",
+    image: "https://i.pinimg.com/736x/2e/8a/48/2e8a4869f19f6a35be05fe6ae3942829.jpg",
     rating: 4.6,
   },
   {
@@ -50,7 +50,7 @@ const products = [
     price: 168,
     description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
     category: "jewelery",
-    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
+    image: "https://i.pinimg.com/1200x/78/fa/f8/78faf88f2ac0cfbc3523038d96d8af4a.jpg",
     rating: 3.,
   },
   {
@@ -59,7 +59,7 @@ const products = [
     price: 9.99,
     description: "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
     category: "jewelery",
-    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png",
+    image: "https://i.pinimg.com/1200x/27/0c/d8/270cd8598b2491b61c94e3b18f8d8ecc.jpg",
     rating: 3,
   },
   {
@@ -68,7 +68,7 @@ const products = [
     price: 10.99,
     description: "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
     category: "jewelery",
-    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
+    image: "https://i.pinimg.com/1200x/be/5e/33/be5e339c5e1ea70f9e87cf8b6b387175.jpg",
     rating: 1.9,
   },
   {
@@ -77,7 +77,7 @@ const products = [
     price: 64,
     description: "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_t.png",
+    image: "https://i.pinimg.com/736x/ff/e9/a2/ffe9a2a38aad07a078f7d310c3ed29ff.jpg",
     rating: 3.3,
   },
   {
@@ -86,7 +86,7 @@ const products = [
     price: 109,
     description: "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_t.png",
+    image: "https://i.pinimg.com/1200x/23/86/02/23860252cd11293bfd4ba2933f4b95d1.jpg",
     rating: 2.9,
   },
   {
@@ -95,16 +95,16 @@ const products = [
     price: 109,
     description: "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_t.png",
+    image: "https://i.pinimg.com/736x/99/cc/5d/99cc5dacebf94557fbc047d6843942b8.jpg",
     rating: 4.8,
   },
   {
     id: 12,
-    title: "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    title: "AMD Ryzen 5 5600 6 cores 12 threads Processor",
     price: 114,
-    description: "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
+    description: "The AMD Ryzen 5 5600 is a powerful 6-core, 12-thread desktop processor built for smooth gaming and fast multitasking. It delivers excellent speed, power efficiency, and value for PC builders.",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+    image: "https://img.drz.lazcdn.com/static/pk/p/6a38b215525e86875f80b2e36e68d02e.jpg_720x720q80.jpg_.webp",
     rating: 4.8,
   },
   {
@@ -113,7 +113,7 @@ const products = [
     price: 599,
     description: "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_t.png",
+    image: "https://i.pinimg.com/736x/54/6c/a8/546ca88cc9a8c3809055ca754e561701.jpg",
     rating: 2.9,
   },
   {
@@ -122,7 +122,7 @@ const products = [
     price: 999.99,
     description: "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_t.png",
+    image: "https://i.pinimg.com/736x/de/74/d2/de74d2f1dd2ba71a0b25a612dace6104.jpg",
     rating: 2.2,
   },
   {
@@ -131,7 +131,7 @@ const products = [
     price: 56.99,
     description: "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_t.png",
+    image: "https://i.pinimg.com/1200x/f6/75/f9/f675f9454565f77cc6195f2839f31720.jpg",
     rating: 2.6,
   },
   {
@@ -140,7 +140,7 @@ const products = [
     price: 29.95,
     description: "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_t.png",
+    image: "https://i.pinimg.com/736x/0d/d6/6e/0dd66e5e67a67cd853ff3841c05506c1.jpg",
     rating: 2.9,
   },
   {
@@ -149,7 +149,7 @@ const products = [
     price: 39.99,
     description: "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2t.png",
+    image: "https://i.pinimg.com/1200x/0d/15/b5/0d15b5e4b447337bb345a8653045f36b.jpg",
     rating: 3.8,
   },
   {
@@ -158,7 +158,7 @@ const products = [
     price: 9.85,
     description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_t.png",
+    image: "https://i.pinimg.com/1200x/ff/52/43/ff5243a3e9ffb02c73de109db6a77bd2.jpg",
     rating: 4.7,
   },
   {
@@ -167,7 +167,7 @@ const products = [
     price: 7.95,
     description: "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_t.png",
+    image: "https://i.pinimg.com/736x/11/7d/c5/117dc505b998984cdabf676ba442c574.jpg",
     rating: 4.5,
   },
   {
@@ -176,7 +176,7 @@ const products = [
     price: 12.99,
     description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png",
+    image: "https://i.pinimg.com/1200x/6c/19/b6/6c19b6a73fb50ba08dc3d88935a33d44.jpg",
     rating: 3.6,
   },
   {
